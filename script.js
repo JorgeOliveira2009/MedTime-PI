51b43e3e43eb8f771b44dd82d4231bc995bdcfe5
j.j olveira 2009 <jorjinhocorrea@gmail.com>
2026-03-20 13:51:29 -0300

    feat:função principal do app, lembretes

--- script.js
+++ script.js
@@ -84,3 +84,71 @@ function fazerLogin() {
 function irParaCadastro() {
     window.location.href = "cadastro.html";
 }
+
+
+//app
+
+if (localStorage.getItem("logado") !== "true") {
+    window.location.href = "login.html";
+}
+
+
+window.onload = carregarLembretes;
+
+function adicionarLembrete() {
+    const nome = document.getElementById("nomeRemedio").value;
+    const horario = document.getElementById("horario").value;
+
+    if (!nome || !horario) {
+        alert("Preencha o nome do remédio e o horário!");
+        return;
+    }
+
+    const lembrete = {
+        nome: nome,
+        horario: horario,
+        tomado: false
+    };
+
+    let lembretes = JSON.parse(localStorage.getItem("lembretes")) || [];
+    lembretes.push(lembrete);
+
+    localStorage.setItem("lembretes", JSON.stringify(lembretes));
+
+    document.getElementById("nomeRemedio").value = "";
+    document.getElementById("horario").value = "";
+
+    carregarLembretes();
+}
+
+function carregarLembretes() {
+    const lista = document.getElementById("lista");
+    lista.innerHTML = "";
+
+    let lembretes = JSON.parse(localStorage.getItem("lembretes")) || [];
+
+    lembretes.forEach((lembrete, index) => {
+        if (!lembrete.tomado) {
+            const item = document.createElement("li");
+            item.innerHTML = `
+                ${lembrete.nome} - ${lembrete.horario}
+                <button onclick="marcarComoTomado(${index})">Já tomei</button>
+            `;
+            lista.appendChild(item);
+        }
+    });
+}
+
+function marcarComoTomado(index) {
+    let lembretes = JSON.parse(localStorage.getItem("lembretes")) || [];
+    lembretes[index].tomado = true;
+
+    localStorage.setItem("lembretes", JSON.stringify(lembretes));
+    carregarLembretes();
+}
+
+// Logout
+function logout() {
+    localStorage.removeItem("logado");
+    window.location.href = "login.html";
+}
